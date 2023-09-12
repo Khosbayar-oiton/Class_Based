afd44a3c1afd2e8f0fafe30f68428fbcb94288d4
Oni <khosbayar@oiton.org>
2023-09-12 18:59:34 +0800

    <Function based need to change to Class based>

--- register/urls.py
+++ register/urls.py
@@ -2,8 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.class_list, name='class_list'),
-    path('class/<int:pk>/', views.class_detail, name='class_detail'),
-    path('class/new/', views.class_new, name='class_new'),
-    
+    path("", views.class_list, name="class_list"),
+    path("class/<int:pk>/", views.class_detail, name="class_detail"),
+    path("class/new/", views.class_new, name="class_new"),
+    path("class/edit/<int:pk>/", views.class_edit, name="class_edit"),
+    path("class/delete/<int:pk>/", views.class_delete, name="class_delete"),
+    path("students/", views.student_list, name="student_list"),
+    path("students/<int:pk>/", views.student_detail, name="student_detail"),
+    path("students/new/", views.student_new, name="student_new"),
+    path("students/edit/<int:pk>/", views.student_edit, name="student_edit"),
+    path("students/delete/<int:pk>/", views.student_delete, name="student_delete"),
 ]
--- register/views.py
+++ register/views.py
@@ -6,87 +6,85 @@ from .forms import ClassForm, StudentForm
 
 def class_list(request):
     classes = Class.objects.all()
-    return render(request, 'register/class_list.html', {'classes': classes})
+    return render(request, "register/class_list.html", {"classes": classes})
+
 
 def class_detail(request, pk):
     class_instance = get_object_or_404(Class, pk=pk)
-    return render(request, 'register/class_detail.html', {'class_instance': class_instance})
+    students = class_instance.student_set.all()
+    return render(
+        request,
+        "register/class_detail.html",
+        {"class_instance": class_instance, "students": students},
+    )
+
 
 def class_new(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = ClassForm(request.POST)
         if form.is_valid():
             class_instance = form.save()
-            return redirect('class_detail', pk=class_instance.pk)
+            return redirect("class_detail", pk=class_instance.pk)
     else:
         form = ClassForm()
-    return render(request, 'register/class_edit.html', {'form': form})
-
-# def class_create(request):
-#     if request.method == 'POST':
-#         form = ClassForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('class_list')
-#     else:
-#         form = ClassForm()
-#     return render(request, 'class_form.html', {'form': form})
-
-# def class_update(request, pk):
-#     class_instance = get_object_or_404(Class, pk=pk)
-#     if request.method == 'POST':
-#         form = ClassForm(request.POST, instance=class_instance)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('class_list')
-#     else:
-#         form = ClassForm(instance=class_instance)
-#     return render(request, 'class_form.html', {'form': form})
-
-# def class_delete(request, pk):
-#     class_instance = get_object_or_404(Class, pk=pk)
-#     if request.method == 'POST':
-#         class_instance.delete()
-#         return redirect('class_list')
-#     return render(request, 'class_confirm_delete.html', {'class_instance': class_instance})
+    return render(request, "register/class_new.html", {"form": form})
 
 
-# def student_list(request):
-#     students = Student.objects.all()
-#     return render(request, 'student_list.html', {'students': students})
+def class_edit(request, pk):
+    class_instance = get_object_or_404(Class, pk=pk)
+    if request.method == "POST":
+        form = ClassForm(request.POST, instance=class_instance)
+        if form.is_valid():
+            class_instance = form.save()
+            return redirect("class_detail", pk=class_instance.pk)
+    else:
+        form = ClassForm(instance=class_instance)
+    return render(request, "register/class_edit.html", {"form": form})
 
-# # Detail view for a single student
-# def student_detail(request, pk):
-#     student = get_object_or_404(Student, pk=pk)
-#     return render(request, 'register/student_detail.html', {'student': student})
 
-# # Create view for adding a new student
-# def student_create(request):
-#     if request.method == 'POST':
-#         form = StudentForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('student_list')
-#     else:
-#         form = StudentForm()
-#     return render(request, 'student_form.html', {'form': form})
+def class_delete(request, pk):
+    class_instance = get_object_or_404(Class, pk=pk)
+    class_instance.delete()
+    return redirect("class_list")
 
-# # Update view for editing an existing student
-# def student_update(request, pk):
-#     student = get_object_or_404(Student, pk=pk)
-#     if request.method == 'POST':
-#         form = StudentForm(request.POST, instance=student)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('student_list')
-#     else:
-#         form = StudentForm(instance=student)
-#     return render(request, 'student_form.html', {'form': form})
 
-# # Delete view for removing a student
-# def student_delete(request, pk):
-#     student = get_object_or_404(Student, pk=pk)
-#     if request.method == 'POST':
-#         student.delete()
-#         return redirect('student_list')
-#     return render(request, 'student_confirm_delete.html', {'student': student})
+#########################################################################################################################
+
+
+def student_detail(request, pk):
+    student_instance = get_object_or_404(Student, pk=pk)
+    return render(
+        request, "register/student_detail.html", {"student_instance": student_instance}
+    )
+
+
+def student_list(request):
+    students = Student.objects.all()
+    return render(request, "register/class_detail.html", {"students": students})
+
+
+def student_new(request):
+    if request.method == "POST":
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            student_instance = form.save()
+            return redirect("student_detail", pk=student_instance.pk)
+    else:
+        form = StudentForm()
+    return render(request, "register/student_new.html", {"form": form})
+
+def student_edit(request, pk):
+    student_instance = get_object_or_404(Student, pk=pk)
+    if request.method == "POST":
+        form = StudentForm(request.POST, instance=student_instance)
+        if form.is_valid():
+            student_instance = form.save()
+            return redirect("class_detail", pk=student_instance.stud_Class.pk)
+    else:
+        form = StudentForm(instance=student_instance)
+    return render(request, "register/student_edit.html", {"form": form})
+
+def student_delete(request, pk):
+    student_instance = get_object_or_404(Student, pk=pk)
+    student_instance.delete()
+    return redirect('class_detail', pk=student_instance.stud_Class.pk)
